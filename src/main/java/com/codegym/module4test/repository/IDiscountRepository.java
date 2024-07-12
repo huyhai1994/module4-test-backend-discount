a6406c763dbdbcd2fc243e06b8410eb5c68b02f9
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-12 11:59:01 +0700

    update search

--- src/main/java/com/codegym/module4test/repository/IDiscountRepository.java
+++ src/main/java/com/codegym/module4test/repository/IDiscountRepository.java
@@ -2,6 +2,11 @@ package com.codegym.module4test.repository;
 
 import com.codegym.module4test.entity.Discount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.math.BigDecimal;
 
 public interface IDiscountRepository extends JpaRepository<Discount, Long> {
+    @Query(value = "SELECT * FROM discounts WHERE discount_money >= :discountmoney", nativeQuery = true)
+    Iterable<Discount> getDiscountsByDiscountMoneyGreaterThanEqual(BigDecimal discountMoney);
 }
